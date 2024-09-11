9e8d050943bf255844f842eec40aa059c1ae3d25
Ali Arda Akkız <111060413+ardaakkiz@users.noreply.github.com>
2024-09-11 13:15:57 +0300

    Update FilmCard.tsx
    updated watch button

--- src/components/FilmCard.tsx
+++ src/components/FilmCard.tsx
@@ -50,7 +50,7 @@ const FilmCard = ({ film }: { film: Film }) => {
             <div className="flex space-x-2">
                 <Link href="/video" className="w-full">
                     <button className="flex items-center justify-center px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600 w-full">
-                        <PlayIcon href="/video" className="h-4 w-4 mr-1" />
+                        <PlayIcon className="h-4 w-4 mr-1" />
                         <span>Watch</span>
                     </button>
                 </Link>
